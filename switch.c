c5f70dcd1bf91cb476d583e2e3497a3d8400ede2
rvsrigayathri <r.v.srigayathri@gmail.com>
2026-02-17 13:48:12 +0530

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int day=5;
+    switch (day)
+    {
+        case 0:
+            printf("Marvalous morning to all");
+            break;
+        case 1:
+            printf("Truthful morning to all");
+            break;
+        case 2:
+            printf("Wonderful morning to all");
+            break;
+        case 3:
+            printf("Truthful morning to all");
+            break;
+        case 4:
+            printf("Fantastic morning to all");
+            break;
+        case 5:
+            printf("Spiritful morning to all");
+            break;
+        default:
+            printf("Invalid statement");
+    }
+    return 0;
