6e8f7d6bb92dfafec0032d9d67e9147fd49f4593
R.Srigayathri <r.v.srigayathri@gmail.com>
2026-02-28 12:10:06 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int n, i;
+    long long fact = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if(n < 0) {
+        printf("Factorial not defined");
+    }
+    else {
+        for(i = 1; i <= n; i++)
+            fact *= i;
+
+        printf("Factorial = %lld", fact);
+    }
+
+    return 0;
+}
