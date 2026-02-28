1380bb633f0ac6753a4902194577ee699ec6913d
R.Srigayathri <r.v.srigayathri@gmail.com>
2026-02-28 12:26:02 +0530

    Add files via upload

---
+++ alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    char ch;
+    scanf(" %c", &ch);
+
+    if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+        printf("Alphabet");
+    else
+        printf("Not an Alphabet");
+
+    return 0;
+}
